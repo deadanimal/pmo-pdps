62d1f83ef8c6e05503f4675cc2b6c8e7057ada82
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-20 22:50:13 +0800

    asdasdasdsacxcasd

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,18 +4,23 @@ import { Router, NavigationEnd } from "@angular/router";
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.scss"]
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
   isCollapsed = true;
+
+  // slider
+  slider1 = "assets/img/brand/banner.jpeg";
+  slider2 = "assets/img/brand/banner2.png";
+
   constructor(private router: Router) {
-    router.events.subscribe(val => {
+    router.events.subscribe((val) => {
       this.isCollapsed = true;
     });
   }
-  mobileView(){
+  mobileView() {
     if (window.innerWidth < 992) {
-        return true;
+      return true;
     }
     return false;
   }
