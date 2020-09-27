be6ec757f807de3ddd0655f1b82a2b20f5160964
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-27 21:35:36 +0800

    adsadasdad

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
-  styleUrls: ["./footer.component.scss"]
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent implements OnInit {
   date: Date = new Date();
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  navigatePage(path: String) {
+    // let qq = "db17a36a-1da6-4919-9746-dfed8802ec9d";
+    if (path == "agency/programs") {
+      return this.router.navigate([path]);
+    }
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import Glide from "@glidejs/glide";
 
 @Component({
   selector: "app-navbar",
@@ -12,6 +13,9 @@ export class NavbarComponent implements OnInit {
   // slider
   slider1 = "assets/img/brand/banner.jpeg";
   slider2 = "assets/img/brand/banner2.png";
+  slider3 = "assets/img/brand/banner3.png";
+  slider4 = "assets/img/brand/banner4.png";
+  imgAvatar = "assets/img/faces/christian.jpg";
 
   constructor(private router: Router) {
     router.events.subscribe((val) => {
@@ -24,7 +28,25 @@ export class NavbarComponent implements OnInit {
     }
     return false;
   }
-  ngOnInit() {}
+  ngOnInit() {
+    // Carousel
+    new Glide(".glide", {
+      type: "carousel",
+      startAt: 0,
+      focusAt: 2,
+      perTouch: 1,
+      perView: 4,
+    }).mount();
+
+    // Testimonial Carousel
+    new Glide(".testimonial-glide", {
+      type: "carousel",
+      startAt: 0,
+      focusAt: 2,
+      perTouch: 1,
+      perView: 4,
+    }).mount();
+  }
 
   navigatePage(path: String) {
     // let qq = "db17a36a-1da6-4919-9746-dfed8802ec9d";
